5670d7375e30043fbff51f5e72a415e9d475724c
thisismyname <98567847+CoderConspiracy@users.noreply.github.com>
2022-02-04 15:41:26 +0700

    Update textbasedgamae.py

--- textbasedgamae.py
+++ textbasedgamae.py
@@ -1,9 +1,10 @@
-#This is made by NhatCorporation.
+#haha code go yeet
 print('----------text based game----------')
 name = input('What is your name?\n')
 choice1 = input('Press 0( or some int, idk ) to continue, or 1 to fuck off.\n')
 if choice1 == '1':
     print('Ok now fuck off.')
+    print('"outta here" ending. 1/10 endings.')
 else:
     print('OK. Program starting......')
     print('You wake up in a bed in a wooden house.') 
@@ -13,27 +14,46 @@ else:
     if choice2=='0':
         print('You go out.You died of emotional damage. You suck.')
         print('Your stats: 0 HP, 0 DEF, 0 MP, 0 Coin.')
-        print('EMOTIONAL DAMAGE ENDING. 1/6 endings.')
+        print('EMOTIONAL DAMAGE ENDING. 2/10 endings.')
     elif choice2=='1':
         print('You keep sleeping. You died of unemployment. You suck.')
         print('Your stats: 0 HP, 0 DEF, 0 MP, 0 Coin.')
-        print('LAZY ENDING. 2/6 endings')
+        print('LAZY ENDING. 3/10 endings')
+        print('2308.*')
     elif choice2=='666':
         print('You found the secret gateway to hell. You came inside, and meet Hades himself.')
         choiceSecret = input('Press 1 to talk to him, or 0 to fight with him.\n')
         if choiceSecret == '0':
             print('You fight with him. You win with 1 HP left. Turns out, Hades is very weak, and he could not fight a mortal human like you')
             print('You take his throne, and become the god of hell,',name,'.')
-            print('666T6H6R6O6N6E666E6N6D6I6N6G666. 3/6 endings.')
+            print('666T6H6R6O6N6E666E6N6D6I6N6G666. 4/10 endings.')
         elif choiceSecret == "1":
             print('You talk to him. You find out that Hades is alone, and he loves you so much that he could insert a 9-inch long hell trident inside of you. So, you become the wife of Hades.')
-            print('WIFE ENDING. 4/6 endings.')
+            print('WIFE ENDING. 5/10 endings.')
         else:
             print('You do not want to do anything above, so you are stuck in a loop, where you CAN NOT ESCAPE.')
-            print('REAL BAD ENDING. 5/6 endings')
+            print('REAL BAD ENDING. 6/10 endings')
+    elif choice2 == '2308':
+        print('You meet Nhat, the creator of the game. You have his code, as you fight a long, tiring fight with him. ')
+        print('c0d:3 3n.d1/n9. 7/10 endings.')
+    elif choice2 =='textbasedgame' :
+        print('You encounter a man with big, burly hair.')
+        choiceSecret2 = input('Press 1 to run away, or 0 to fight.')
+        if choiceSecret2 == '1':
+            print('You run away, but there\'s nowhere to run. You get crushed by the man\'s big, burly hair.')
+            print('CRUSHED ENDING.8/10 endings.')
+        elif choiceSecret2 == '0':
+            print('You decided to fight. But, he doesn\'t want to fight with you. He was waiting for you to give you the weapon to fight the god of hell, Hades.' )
+            print('Weapon Ending. 9/10 endings.')
+            print('https://www.google.com/search?q=666&bih=573&biw=1036&hl=vi&source=hp&ei=r-T8YcinKsvWz7sP64q_sA8&iflsig=AHkkrS4AAAAAYfzyv1VlW5Db5JZpkJAuy6bbxYVYjNig&ved=0ahUKEwiIu-fD0eX1AhVL63MBHWvFD_YQ4dUDCAc&uact=5&oq=666&gs_lcp=Cgdnd3Mtd2l6EAMyCAgAEIAEELEDMgUIABCABDILCC4QgAQQsQMQyQMyBQgAEIAEMgsIABCABBCxAxCDATIFCC4QgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOggIABCxAxCDAToLCC4QgAQQsQMQgwE6CAguEIAEELEDUABYxgJgrQRoAHAAeACAAaQBiAG5ApIBAzIuMZgBAKABAQ&sclient=gws-wiz')
+        else:
+            print('You do not want to do anything above, so you are stuck in a loop, where you CAN NOT ESCAPE.')
+            print('REAL BAD ENDING. 6/10 endings')   
     else:
         print('You fucking broke the game. Fuck off.')
-        print('BAD ENDING. THE FUCKING FINAL ENDING.')
+        print('shitty ending. 10/10 endings.')
+        print('textbasedgame.')
+
 
 
 
@@ -64,6 +84,10 @@ else:
 #if you find these lines of codes, you are a legend.
 #https://www.youtube.com/watch?v=GHMjD0Lp5DY (totally not a rickroll.)
 #guess wat, you can Ctrl + LMB to click this link.
+
+
+
+
 
 
 
