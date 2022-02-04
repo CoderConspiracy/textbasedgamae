e9ea74bc26e0753b1b9d31f51a0ec3dc27667b0f
CoderConspiracy <98567847+CoderConspiracy@users.noreply.github.com>
2022-02-04 11:11:59 +0700

    Add files via upload

---
+++ textbasedgamae.py
@@ -0,0 +1,70 @@
+#This is made by NhatCorporation.
+print('----------text based game----------')
+name = input('What is your name?\n')
+choice1 = input('Press 0( or some int, idk ) to continue, or 1 to fuck off.\n')
+if choice1 == '1':
+    print('Ok now fuck off.')
+else:
+    print('OK. Program starting......')
+    print('You wake up in a bed in a wooden house.') 
+    print('The fireplace is burning.') 
+    print('The winter is near, and it is very cold outside.')
+    choice2=input('Press 0 to go out, or 1 to keep sleeping.\n')
+    if choice2=='0':
+        print('You go out.You died of emotional damage. You suck.')
+        print('Your stats: 0 HP, 0 DEF, 0 MP, 0 Coin.')
+        print('EMOTIONAL DAMAGE ENDING. 1/6 endings.')
+    elif choice2=='1':
+        print('You keep sleeping. You died of unemployment. You suck.')
+        print('Your stats: 0 HP, 0 DEF, 0 MP, 0 Coin.')
+        print('LAZY ENDING. 2/6 endings')
+    elif choice2=='666':
+        print('You found the secret gateway to hell. You came inside, and meet Hades himself.')
+        choiceSecret = input('Press 1 to talk to him, or 0 to fight with him.\n')
+        if choiceSecret == '0':
+            print('You fight with him. You win with 1 HP left. Turns out, Hades is very weak, and he could not fight a mortal human like you')
+            print('You take his throne, and become the god of hell,',name,'.')
+            print('666T6H6R6O6N6E666E6N6D6I6N6G666. 3/6 endings.')
+        elif choiceSecret == "1":
+            print('You talk to him. You find out that Hades is alone, and he loves you so much that he could insert a 9-inch long hell trident inside of you. So, you become the wife of Hades.')
+            print('WIFE ENDING. 4/6 endings.')
+        else:
+            print('You do not want to do anything above, so you are stuck in a loop, where you CAN NOT ESCAPE.')
+            print('REAL BAD ENDING. 5/6 endings')
+    else:
+        print('You fucking broke the game. Fuck off.')
+        print('BAD ENDING. THE FUCKING FINAL ENDING.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#wy m i wriing tis cod it i 2am an vey fucing col ouside i ned slep ples hep
+#if you find these lines of codes, you are a legend.
+#https://www.youtube.com/watch?v=GHMjD0Lp5DY (totally not a rickroll.)
+#guess wat, you can Ctrl + LMB to click this link.
+
+
+
+
